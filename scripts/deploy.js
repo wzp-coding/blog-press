a5b54e0ec6bcc5065fed21569d6d279d0f84f79a
wzp-coding <2236277721@qq.com>
2021-08-19 23:54:21 +0800

    'deploy'

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,17 @@
 const {exec} = require('child_process')
-exec('npm run build',(error,stdout,stderr)=>{
-    if(error){
-        console.log(error)
-        return
-    }
-    console.log('stdout: ', stdout);
+// exec('npm run build',(error,stdout,stderr)=>{
+//     if(error){
+//         console.log(error)
+//         return
+//     }
+//     console.log('stdout: ', stdout);
     exec('cd ../docs/.vuepress/dist',()=>{
         exec('git init',()=>{
             exec('git add -A',()=>{
                 exec("git commit -m 'deploy'",()=>{
-                    exec('git push -f git@github.com:wzp-coding/wzp-coding.github.io.git master',()=>{
+                    exec('git push -f git@github.com:wzp-coding/wzp-coding.github.io.git master',(err,stdout,stderr)=>{
+                        console.log('err: ', err);
+                        console.log('stdout: ', stdout);
                         console.log('deploy success');
                         exec('cd -')
                     })
@@ -17,4 +19,4 @@ exec('npm run build',(error,stdout,stderr)=>{
             })
         })
     })
-})
+// })
