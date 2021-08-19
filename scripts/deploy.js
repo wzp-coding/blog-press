cc09db6d2d2caae65fc9342569620b73b63786b9
wzp-coding <2236277721@qq.com>
2021-08-20 00:08:26 +0800

    'deploy'

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,29 @@
-const {exec} = require('child_process')
-// exec('npm run build',(error,stdout,stderr)=>{
-//     if(error){
-//         console.log(error)
-//         return
-//     }
-//     console.log('stdout: ', stdout);
-    exec('cd ../docs/.vuepress/dist',()=>{
-        exec('git init',()=>{
-            exec('git add -A',()=>{
-                exec("git commit -m 'deploy'",()=>{
-                    exec('git push -f git@github.com:wzp-coding/wzp-coding.github.com.git master',(err,stdout,stderr)=>{
-                        console.log('err: ', err);
-                        console.log('stdout: ', stdout);
-                        console.log('deploy success');
-                        exec('cd -')
-                    })
-                })
-            })
-        })
-    })
-// })
+const { exec } = require("child_process");
+// exec("npm run build", (error, stdout, stderr) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   console.log(stdout);
+// });
+exec("cd ../docs/.vuepress/dist", () => {
+  exec("git init", () => {
+    exec("git add -A", () => {
+      exec("git commit -m 'deploy'", () => {
+        exec(
+          "git remote add origin https://github.com/wzp-coding/wzp-coding.github.com.git",
+          (err, stdout, stderr) => {
+            exec("git push --set-upstream origin master", (err, stdout) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
+              console.log(stdout);
+              console.log("deploy success");
+            });
+          }
+        );
+      });
+    });
+  });
+});
