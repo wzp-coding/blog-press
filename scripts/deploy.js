30c7fe9bc25245516bc0fa63d570851ac1f89b38
wzp-coding <2236277721@qq.com>
2021-08-20 00:58:36 +0800

    npm run build自动部署

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,34 +1,49 @@
 const { exec } = require("child_process");
-// exec("npm run build", (error, stdout, stderr) => {
-//   if (error) {
-//     console.log(error);
-//     return;
-//   }
-//   console.log(stdout);
-// });
-// exec("cd ", (err, stdout) => {
-  exec("git init",{cwd:'D:\\study-file\\blog-press\\docs\\.vuepress\\dist'}, (err, stdout) => {
-    if (err) {
-      console.log("err: ", err);
-      return;
-    }
-    console.log("stdout: ", stdout);
-    exec("git add -A", () => {
-      exec("git commit -m 'deploy'", () => {
-        exec(
-          "git remote add origin https://github.com/wzp-coding/wzp-coding.github.com.git",
-          (err, stdout, stderr) => {
-            exec("git push --set-upstream origin master", (err, stdout) => {
-              if (err) {
-                console.log(err);
-                return;
-              }
-              console.log(stdout);
-              console.log("deploy success");
-            });
-          }
-        );
-      });
-    });
-  });
-// });
+const cwd = "D:\\study-file\\blog-press\\docs\\.vuepress\\dist";
+
+exec("git init", { cwd }, gitAdd);
+function gitAdd(err, stdout) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+  exec("git add -A", { cwd }, gitCommit);
+}
+function gitCommit(err, stdout) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+  exec("git commit -m 'deploy'", { cwd }, gitRemote);
+}
+function gitRemote(err, stdout) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+  exec(
+    "git remote add origin https://github.com/wzp-coding/wzp-coding.github.com.git",
+    { cwd },
+    gitPush
+  );
+}
+function gitPush(err, stdout, stderr) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+
+  exec("git push origin master -f", { cwd }, finish);
+}
+function finish(err, stdout) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(stdout);
+  console.log("deploy success");
+}
